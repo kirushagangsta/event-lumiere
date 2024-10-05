e97af9edf4cff8ee82df08d764bbfd3565044350
DankaParvan <parvandaniil@gmail.com>
2024-10-06 01:08:37 +0700

    сверстала header

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -3,9 +3,42 @@
 </script>
 
 <template>
-  <div class="fw-700">Тут хедер</div>
+  <header class="width-wrapper header">
+    <div class="header__separator w-100">
+    </div>
+    <div class="d-flex justify-content-between align-items-center mt-16 w-100">
+      <img class="header__logo" src="@/assets/svg/logo.svg" alt="logo">
+      <div class="header-menu d-flex gap-48 color-white text-uppercase title-14-700">
+        <nuxt-link to="/">Парфюмерия</nuxt-link>
+        <nuxt-link to="/">О НАС</nuxt-link>
+        <nuxt-link to="/">ИНФО</nuxt-link>
+        <nuxt-link to="/">Контакты</nuxt-link>
+      </div>
+      <i class="icon icon-basket"></i>
+    </div>
+  </header>
 </template>
 
 <style scoped lang="scss">
+.header {
+  background: black;
+  margin-top: 32px;
 
+  &__separator {
+    background: rgba(255, 255, 255, 0.1);
+    height: 1px;
+  }
+
+  &__logo {
+    height: 44px;
+    width: 146px;
+  }
+
+  &-menu {
+    letter-spacing: 4px;
+    a {
+      color: $white-color;
+    }
+  }
+}
 </style>
