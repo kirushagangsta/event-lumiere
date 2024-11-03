1ff31e573fb6e303f63098ca123c7c39e3936840
KIRILL VOROBEV <kudikyy@gmail.com>
2024-11-03 18:51:31 +0700

    наебашил анимаций

--- components/MainPage/Intro.vue
+++ components/MainPage/Intro.vue
@@ -2,21 +2,29 @@
   <div class="intro w-full h-[971px] flex justify-center">
     <img src="@/assets/images/intro.webp" alt="intro" class="h-[1117px] absolute w-full top-0 left-0 object-cover">
     <div class="intro-content mt-[103px] py-[192px] relative text-center text-white font-cormorant uppercase width-wrapper h-fit">
-      <h1 class="title-1 tracking-[4px]">
+      <h1 class="text-animation title-1 tracking-[4px]">
         Кто знает — тот знает
       </h1>
-      <div class="title-30-600 mt-4">Сообщество ценителей</div>
-      <div class="mx-auto mt-12 mb-0 bg-black py-[15px] px-12 w-fit tracking-[4px] cursor-pointer">Перейти к покупкам</div>
+      <div class="text-animation title-30-600 mt-4">Сообщество ценителей</div>
+      <div class="mx-auto mt-12 mb-0 bg-black py-[15px] px-12 w-fit tracking-[4px] cursor-pointer button-animation">Перейти к покупкам</div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
 <style lang="scss" scoped>
 .intro-content {
   background: radial-gradient(70.71% 70.71% at 50% 50%, rgba(0, 0, 0, 0.41) 8%, rgba(0, 0, 0, 0) 70.4%);
+}
+
+.text-animation {
+  animation: text-slide-in 1s forwards;
+}
+
+.button-animation {
+  opacity: 0;
+  animation: fade-in 1s 1s forwards;
 }
 </style>
